7a9ec304a714fe7898e1f84df24e07265f6ec908
mukhammadrizooff <legend.it@bk.ru>
2022-03-21 23:41:36 +0500

    Modify src files

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import ContinentHeader from './components/ContinentHeader/ContinentHeader';
+import Continents from './components/Continents/Continents';
+import Countries from './components/Countries/Countries';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route
+          path="/"
+          element={(
+            <>
+              <Header />
+              <Continents />
+            </>
+          )}
+        />
+        <Route
+          path="/:continentName"
+          element={(
+            <>
+              <ContinentHeader />
+              <Countries />
+            </>
+          )}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
